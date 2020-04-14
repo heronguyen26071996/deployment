3f462aea9deb4a4c2ef3064bdc072dd6bec88899
heronguyen2607 <heronguyen2607@gmail.com>
2020-04-14 15:18:30 +0700

    Updates

---
+++ precache-manifest.4b2980270d456a11fbfbb777bab29a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a85ba96dace86205c6548e46efc91d",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "7683eed5d6407f6bb3cb",
+    "url": "/deploy-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0568a3aa3fc6fafdc93a",
+    "url": "/deploy-github/static/js/2.b15206df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-github/static/js/2.b15206df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7683eed5d6407f6bb3cb",
+    "url": "/deploy-github/static/js/main.929a5f49.chunk.js"
+  },
+  {
+    "revision": "6fec8640bd7fbb42ae8f",
+    "url": "/deploy-github/static/js/runtime-main.d753ec5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
